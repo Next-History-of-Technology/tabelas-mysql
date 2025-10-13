cedcfe8c678047ebd9ec914bd25d4aa2fe9b0ca7
gustavomot4 <gustavo.mota@sptech.school>
2025-10-13 17:21:44 -0300

    adicionando tabelas dependentes

--- SPRINTDATABASE.sql
+++ SPRINTDATABASE.sql
@@ -9,27 +9,27 @@ idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 CNPJ CHAR(14) UNIQUE NOT NULL,
 nomeFantasia VARCHAR(60) NOT NULL,
 razãoSocial VARCHAR(80) UNIQUE NOT NULL,
-email VARCHAR(50) UNIQUE NOT NULL,
 telefone VARCHAR(14),
 statusEmpresa TINYINT DEFAULT 1
 );
 
 CREATE TABLE Funcionario (
-idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
+idFuncionario INT AUTO_INCREMENT,
 email VARCHAR(50) UNIQUE NOT NULL,
 nome VARCHAR(60) NOT NULL,
 senha VARCHAR(100) NOT NULL,
 telefone CHAR(13) UNIQUE NOT NULL,
 cpf VARCHAR(11) UNIQUE NOT NULL ,
 statusFuncionario TINYINT DEFAULT 1,
-fkEmpresa INT UNIQUE,
+PRIMARY KEY(idFuncionario , fkEmpresa),
+fkEmpresa INT NOT NULL,
 CONSTRAINT fkEmpresaFuncionario FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE Sensor(
-idSensor INT PRIMARY KEY AUTO_INCREMENT,
+idSensor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 codSerie INT NOT NULL UNIQUE,
-statusSensor TINYINT DEFAULT 1,
+statusSensor TINYINT DEFAULT 1 NOT NULL,
 dtInstalacao DATE,
 localSensor VARCHAR(45),
 fkEmpresa INT,
@@ -38,50 +38,49 @@ CONSTRAINT fkLocalEmpresa FOREIGN KEY (fkEmpresa)
 );
 
 CREATE TABLE Leitura (
-	idLeitura INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	idLeitura INT NOT NULL AUTO_INCREMENT,
     dataHora  DATETIME DEFAULT CURRENT_TIMESTAMP,
     valorPPM  DECIMAL (5,2) NOT NULL,
-	fkSensor INT,
+	fkSensor INT NOT NULL,
+	PRIMARY KEY(idLeitura, fkSensor),
     CONSTRAINT fkLeituraSensor FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor)
 );
 
-CREATE TABLE Plano (
-idPlano INT PRIMARY KEY AUTO_INCREMENT,
-dtVencimento DATE,
-valorPlano DECIMAL (7,2),
-tipoPlano VARCHAR(15),
-CONSTRAINT chkTipoPlano
-	CHECK (tipoPlano IN ('semestral','anual')),
-fkEmpresa INT,
-CONSTRAINT fkEmpresaPlano FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
-);
+
 
 CREATE TABLE Pagamento (
-idPagamento INT PRIMARY KEY AUTO_INCREMENT,
+idPagamento INT AUTO_INCREMENT,
+fkPlano INT,
+dtVencimento DATE NOT NULL,
+valorPlano DECIMAL (7,2) NOT NULL,
+tipoPlano VARCHAR(15) NOT NULL,
+PRIMARY KEY (idPagamento , fkPlano),
 formaPagamento VARCHAR(30) NOT NULL,
 CONSTRAINT chkFormaPagamento
 	CHECK (formaPagamento IN ('boleto', 'credito', 'pix', 'transferencia')),
 dtPagamento DATETIME DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT chkTipoPlano
+	CHECK (tipoPlano IN ('semestral','anual')),
 fkEmpresa INT,
-CONSTRAINT fkEmpresaPagamento FOREIGN KEY(fkEmpresa) REFERENCES Empresa (idEmpresa)  
+CONSTRAINT fkEmpresaPagamento FOREIGN KEY(fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
-INSERT INTO Empresa (CNPJ, nomeFantasia ,razãoSocial, email, telefone) VALUES
-('42599543000117', 'Itau' , 'Itau Unibanco', 'itaubanco@empresarial', '11957451375'),
-('84224447000134', 'Casas Bahia' , 'Casas Bahia', 'casasbahia@vendas', '11959441622'),
-('00000000000001', 'Btg Pactual' ,'Btg Pactual', 'btgpactual@empresarial', '11932111611'),
-('00000000000002', 'Americanas' ,'Americanas', 'americanas@vendas', '11955555637'),
-('00000000000003', 'Avanade' , 'Avanade', 'avanade@empresarial', '11999991637');
+INSERT INTO Empresa (CNPJ, nomeFantasia ,razãoSocial, telefone) VALUES
+('42599543000117', 'Itau' , 'Itau Unibanco',  '11957451375'),
+('84224447000134', 'Casas Bahia' , 'Casas Bahia', '11959441622'),
+('00000000000001', 'Btg Pactual' ,'Btg Pactual',  '11932111611'),
+('00000000000002', 'Americanas' ,'Americanas', '11955555637'),
+('00000000000003', 'Avanade' , 'Avanade', '11999991637');
     
 SELECT * FROM Empresa; 
 
-INSERT INTO Funcionario (email, nome, senha, telefone, cpf) VALUES
-('eduardo.nascimento@sptech.school', 'Eduardo Nascimento', 'Urubu100', '11937061684', '33688622666'),
-('lucas.peres@sptech.school', 'Lucas Peres', 'Lp_2006', '11973239898', '55708075856'),
-('arthur.rfreitas@sptech.scholl', 'Arthur Rodrigues', 'test0000', '11000000000', '10000000000'),
-('cintia.azevedo@sptech.scholl', 'Cintia Miranda', 'test0001', '11000000001', '10000000001'),
-('everton.silva@sptech.scholl', 'Everton Barbosa', 'test0002', '11000000002', '10000000002'),
-('igor.fonseca@sptech.scholl', 'Igor Ruy', 'test0003', '11000000003', '10000000003');
+INSERT INTO Funcionario (fkEmpresa, email, nome, senha, telefone, cpf) VALUES
+(1,'eduardo.nascimento@sptech.school', 'Eduardo Nascimento', 'Urubu100', '11937061684', '33688622666'),
+(1,'lucas.peres@sptech.school', 'Lucas Peres', 'Lp_2006', '11973239898', '55708075856'),
+(1,'arthur.rfreitas@sptech.school', 'Arthur Rodrigues', 'test0000', '11000000000', '10000000000'),
+(2,'cintia.azevedo@sptech.school', 'Cintia Miranda', 'test0001', '11000000001', '10000000001'),
+(2,'everton.silva@sptech.school', 'Everton Barbosa', 'test0002', '11000000002', '10000000002'),
+(3,'igor.fonseca@sptech.school', 'Igor Ruy', 'test0003', '11000000003', '10000000003');
 
 SELECT * FROM Funcionario; 
 
@@ -102,22 +101,13 @@ INSERT INTO Leitura(valorPPM, fkSensor) VALUES
 
 SELECT * FROM Leitura;
 
-INSERT Plano (dtVencimento, valorPlano, tipoPlano, fkEmpresa) VALUES
-('2024-12-30' , 10000 , 'anual' ,  1),
-('2026-02-20' , 15000 , 'anual' ,  2),
-('2026-01-10' , 12000 , 'anual' ,  3),
-('2025-09-30' , 7000 , 'semestral' ,  4),
-('2024-09-20' , 3000 , 'semestral' ,  5);
 
-SELECT * FROM Plano;
-
-
-INSERT INTO Pagamento (formaPagamento , fkEmpresa)VALUES
-('transferencia' , 1),
-('pix' , 2),
-('credito' , 3),
-('transferencia' , 4),
-('transferencia' , 4);
+INSERT INTO Pagamento (fkPlano,  formaPagamento , fkEmpresa , dtVencimento, tipoPlano, valorPlano)VALUES
+(1,'transferencia' , 1 , 2025-12-12 , 'Anual' , 1000),
+(2,'pix' , 2 , 2025-12-12 , 'Anual' , 1000),
+(1,'credito' , 3 , 2025-12-12 , 'Anual' , 1000),
+(1,'transferencia' , 4 , 2025-12-12 , 'Anual' , 1000),
+(2,'transferencia' , 4 , 2025-12-12 , 'Anual' , 1000);
 
 SELECT * FROM Pagamento;
 
@@ -135,3 +125,4 @@ SELECT p.dtPagamento AS 'Data do Pagamento',
         FROM Pagamento AS p
         JOIN Empresa AS e ON p.fkEmpresa = e.idEmpresa
         JOIN Plano AS pl ON pl.fkEmpresa = e.idEmpresa;
+
